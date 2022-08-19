38fb58e1e47050e23b795742caa5eadd70252711
jihun32 <jihun32@naver.com>
2022-08-19 21:08:48 +0900

    ADD: 프로젝트 생성

---
+++ PickUp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  PickUp
+//
+//  Created by 정지훈 on 2022/08/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
