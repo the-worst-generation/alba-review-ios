efe2db5a6c9549f1989717526eae20bcf1209856
jihun32 <jihun32@naver.com>
2023-03-30 15:13:26 +0900

    [Feat] HomeVC UI 구현

--- AlbaReview/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
+++ AlbaReview/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
@@ -7,22 +7,72 @@
 
 import UIKit
 import CoreLocation
-import NMapsMap
 
+import NMapsMap
+import Then
+import SnapKit
 
 class HomeViewController: UIViewController {
 
+    var searchButton: UIButton!
+    let currentLocationButton = UIButton().then {
+        $0.setImage(UIImage(named: "currentLocation"), for: .normal)
+    }
     
     let locationService = LocationManager.shared
     override func viewDidLoad() {
         super.viewDidLoad()
         
         createMapView()
+        configureButton()
+        setUpUI()
+        setAddView()
+        setConstraints()
     }
     
     private func createMapView() {
-        let mapView = NMFNaverMapView(frame: view.frame)
+        let mapView = NMFMapView(frame: view.frame)
         view.addSubview(mapView)
+    }
+
+    //MARK: - SetUp
+    private func setUpUI() {
+        
+    }
+    
+    private func setAddView() {
+        [
+            searchButton,
+            currentLocationButton
+        ].forEach { view.addSubview($0) }
+    }
+    
+    private func setConstraints() {
+        searchButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(50)
+            make.top.equalToSuperview().inset(70)
+        }
+        
+        currentLocationButton.snp.makeConstraints { make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-60)
+            make.trailing.equalToSuperview().inset(20)
+        }
+    }
+
+    private func configureButton() {
+        
+        var config = UIButton.Configuration.plain()
+        var attrTitle = AttributedString.init("구, 동, 장소, 주소 검색")
+        //attrTitle.foregroundColor = .systemGray5
+        
+        config.attributedTitle = attrTitle
+        config.background.backgroundColor = .white
+        config.image = UIImage(systemName: "magnifyingglass")
+        config.imagePlacement = .trailing
+        config.imagePadding = 70
+        config.cornerStyle = .large
+        config.baseForegroundColor = .black
+        searchButton = UIButton(configuration: config)
     }
 }
 
