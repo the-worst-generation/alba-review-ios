fdb01b549dee2684189368c75458c82d017be8d8
jihun32 <jihun32@naver.com>
2023-04-02 23:47:35 +0900

    [SetUp] Cosmos 라이브러리 추가

---
+++ AlbaReview/Source/Presentation/Tab/Review/View/WritableReviewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WritableReviewCell.swift
+//  AlbaReview
+//
+//  Created by 정지훈 on 2023/04/02.
+//
+
+import UIKit
+
+class WritableReviewCell: UICollectionViewCell {
+    
+}
---
+++ AlbaReview/Source/Presentation/Tab/Review/ViewModel/WritableReviewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WritableReviewViewModel.swift
+//  AlbaReview
+//
+//  Created by 정지훈 on 2023/04/02.
+//
+
+import Foundation
