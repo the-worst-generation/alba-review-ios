c5b34adac416543f11f00887c44560cc77c39ed4
jihun32 <jihun32@naver.com>
2023-04-04 17:44:49 +0900

    [Feat] 리뷰, 별점 작성 페이지 UI 임시 구현

--- AlbaReview/Source/Presentation/Tab/Review/WriteReview/ViewController/WriteReviewRatingViewController.swift
+++ AlbaReview/Source/Presentation/Tab/Review/WriteReview/ViewController/WriteReviewRatingViewController.swift
@@ -7,12 +7,50 @@
 
 import UIKit
 
+import Cosmos
+import Then
+import SnapKit
+import RxCocoa
+import RxSwift
+
 class WriteReviewRatingViewController: UIViewController {
+    
+    let reviewTextView = UITextView(frame: .zero).then {
+        $0.textColor = .lightGray
+        $0.layer.borderColor = UIColor.systemGray2.cgColor
+        $0.text = "이 알바에 대한 총평을 남겨주세요."
+        $0.layer.cornerRadius = 5
+        $0.layer.borderWidth = 1
+    }
+    
+    let reviewCosmosView = CosmosView().then {
+        $0.rating = 3
+        $0.text = "3"
+        $0.settings.textMargin = 10
+        $0.settings.textFont = .systemFont(ofSize: 18)
+        $0.settings.filledColor = .systemYellow
+        $0.settings.starSize = 30
+        $0.settings.starMargin = 5
+        $0.settings.fillMode = .precise
+    }
+    
+    let nextButton = UIButton().then {
+        $0.isEnabled = false
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = .systemGray2
+        $0.setTitle("확인", for: .normal)
+        
+    }
+    
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super .viewDidLoad()
         
         setUpUI()
+        setAddView()
+        setConstraints()
+        bind()
     }
     
     //MARK: - SetUp
@@ -22,11 +60,46 @@ class WriteReviewRatingViewController: UIViewController {
     }
     
     private func setAddView() {
-        
+        [
+            reviewTextView,
+            reviewCosmosView,
+            nextButton
+        ]
+            .forEach { view.addSubview($0) }
     }
     
     private func setConstraints() {
+        reviewTextView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(30)
+            make.height.equalTo(300)
+        }
         
+        reviewCosmosView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(reviewTextView.snp.bottom).offset(10)
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(80)
+            make.bottom.equalToSuperview()
+        }
+        
+    }
+    
+    private func bind() {
+        
+        reviewCosmosView.rx.didTouchCosmos
+            .onNext { rating in
+                self.reviewCosmosView.text = String(format: "%.1f", rating)
+            }
+        
+        reviewTextView.rx.didBeginEditing
+            .bind(onNext: { _ in
+                self.reviewTextView.text = ""
+                self.reviewTextView.textColor = .black
+            }).disposed(by: disposeBag)
     }
 
 }
