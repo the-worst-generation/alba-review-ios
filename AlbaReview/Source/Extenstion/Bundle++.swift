a8ca72f349d4c5439f7ddc17f43ec21481ff2696
jihun32 <jihun32@naver.com>
2023-04-18 13:44:04 +0900

    [Feat] API KEY ignore 추가

---
+++ AlbaReview/Source/Extenstion/Bundle++.swift
@@ -0,0 +1,23 @@
+//
+//  Bundle++.swift
+//  AlbaReview
+//
+//  Created by 정지훈 on 2023/04/18.
+//
+
+import Foundation
+
+extension Bundle {
+    var KAKAO_SEARCH_API_KEY: String {
+        guard let file = self.path(forResource: "KAKAO_SEARCH_API_KEY", ofType: "plist") else { return "" }
+        
+        // .plist를 딕셔너리로 받아오기
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        
+        // 딕셔너리에서 값 찾기
+        guard let key = resource["KAKAO_SEARCH_API_KEY"] as? String else {
+            fatalError("API_KEY error")
+        }
+        return key
+    }
+}
