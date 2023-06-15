965ee35731a60ce189729890beb0fa22b08c1c09
jihun32 <jihun32@naver.com>
2023-06-15 22:33:16 +0900

    [Feat] 마커 터치 시 뷰 보여주기

--- AlbaReview/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
+++ AlbaReview/Source/Presentation/Tab/Home/ViewController/HomeViewController.swift
@@ -22,6 +22,48 @@ class HomeViewController: UIViewController {
         $0.setImage(UIImage(named: "currentLocation"), for: .normal)
     }
     
+    let placeInfoView: UIView = UIView().then {
+        $0.backgroundColor = .white
+        $0.isHidden = true
+    }
+    
+    let placeNameLabel: UILabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 18)
+    }
+    
+    let heartImageButton: UIButton = UIButton().then {
+        $0.tintColor = .systemRed
+        $0.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+    }
+    
+    let reviewLabel: UILabel = UILabel().then {
+        $0.configureLabel(text: "리뷰 개수", fontSize: 14, color: .black)
+    }
+    
+    let reviewCountLabel: UILabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14)
+    }
+    
+    let starImageView: UIImageView = UIImageView().then {
+        $0.image = UIImage(systemName: "star.fill")
+        $0.tintColor = .systemYellow
+    }
+    
+    let starLabel: UILabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14)
+    }
+    
+    let goodImageView: UIImageView = UIImageView().then {
+        $0.tintColor = .systemRed
+        $0.image = UIImage(systemName: "hand.thumbsup")
+    }
+    
+    let badImageView: UIImageView = UIImageView().then {
+        $0.tintColor = .systemBlue
+        $0.image = UIImage(systemName: "hand.thumbsdown")
+    }
+    
+    
     let locationService = LocationManager.shared
     
     let reviewViewModel = WritableReviewViewModel.shared
@@ -42,6 +84,7 @@ class HomeViewController: UIViewController {
     
     private func createMapView() {
         view.addSubview(mapView)
+        mapView.touchDelegate = self
     }
 
     //MARK: - SetUp
@@ -50,11 +93,24 @@ class HomeViewController: UIViewController {
     }
     
     private func setAddView() {
+        
+        [
+            placeNameLabel,
+            heartImageButton,
+            reviewLabel,
+            reviewCountLabel,
+            starImageView,
+            starLabel,
+            goodImageView,
+            badImageView
+        ]   .forEach { placeInfoView.addSubview($0) }
+        
         [
             mapView,
             searchButton,
-            currentLocationButton
-        ].forEach { view.addSubview($0) }
+            currentLocationButton,
+            placeInfoView
+        ]   .forEach { view.addSubview($0) }
     }
     
     private func setConstraints() {
@@ -67,6 +123,56 @@ class HomeViewController: UIViewController {
         currentLocationButton.snp.makeConstraints { make in
             make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-60)
             make.trailing.equalToSuperview().inset(20)
+        }
+        
+        placeInfoView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(200)
+        }
+        
+        placeNameLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalToSuperview().inset(20)
+        }
+        
+        heartImageButton.snp.makeConstraints { make in
+            make.leading.equalTo(placeNameLabel.snp.trailing).offset(5)
+            make.top.equalToSuperview().inset(20)
+            make.height.width.equalTo(20)
+        }
+        
+        reviewLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalTo(placeNameLabel.snp.bottom).offset(10)
+        }
+        
+        reviewCountLabel.snp.makeConstraints { make in
+            make.leading.equalTo(reviewLabel.snp.trailing).offset(5)
+            make.top.equalTo(reviewLabel.snp.top)
+        }
+        
+        starImageView.snp.makeConstraints { make in
+            make.leading.equalToSuperview().inset(16)
+            make.top.equalTo(reviewLabel.snp.bottom).offset(10)
+            make.width.height.equalTo(20)
+        }
+        
+        starLabel.snp.makeConstraints { make in
+            make.leading.equalTo(starImageView.snp.trailing).offset(5)
+            make.bottom.equalTo(starImageView.snp.bottom)
+        }
+        
+        badImageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(30)
+            make.width.height.equalTo(30)
+        }
+        
+        goodImageView.snp.makeConstraints { make in
+            make.trailing.equalTo(badImageView.snp.leading).offset(-20)
+            make.bottom.equalToSuperview().inset(30)
+            make.width.height.equalTo(30)
         }
     }
 
@@ -105,7 +211,7 @@ class HomeViewController: UIViewController {
             .filter { $0 != nil}
             .compactMap { $0 }
             .bind(onNext: {
-                self.createMarker($0.lattitude, $0.longtitude)
+                self.createMarker($0)
             }).disposed(by: disposeBag)
         
         searchViewModel.selectedPlaceSubject
@@ -117,15 +223,36 @@ class HomeViewController: UIViewController {
     }
 }
 
-extension HomeViewController {
-    func createMarker(_ lattitude: Double, _ longtitude: Double) {
+extension HomeViewController: NMFMapViewTouchDelegate {
+    
+    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+        print("asdf")
+        placeInfoView.isHidden = true
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    func createMarker(_ data: WritableReviewData) {
         let marker = NMFMarker()
-        marker.position = NMGLatLng(lat: lattitude, lng: longtitude)
+        marker.position = NMGLatLng(lat: data.lattitude, lng: data.longtitude)
+        marker.captionText = data.name
         marker.mapView = mapView
+        
+        marker.touchHandler = { (overlay: NMFOverlay) -> Bool in
+            self.setPlaceInfoView(data: data)
+            return true
+        }
     }
     
     func moveToSearchPlace(_ x: Double, y: Double) {
         let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: y, lng: x))
         mapView.moveCamera(cameraUpdate)
+    }
+    
+    func setPlaceInfoView(data: WritableReviewData) {
+        tabBarController?.tabBar.isHidden = true
+        placeInfoView.isHidden = false
+        placeNameLabel.text = data.name
+        reviewCountLabel.text = String(data.reviewCount)
+        starLabel.text = String(data.rating)
     }
 }
